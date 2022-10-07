484b3c3bc88450a8fb5bb4eece68f0dacee2c90a
B1GM6N <114106132+B1GM6N@users.noreply.github.com>
2022-10-07 14:33:34 -0600

    Add files via upload

--- recursion.py
+++ recursion.py
@@ -3,10 +3,12 @@
 #          Peyton Germann           #
 #####################################
 import turtle
-
+# adds turtle
 t = turtle.Turtle()
 end = turtle.Screen()
-
+# function that draws a star
+# calls itself over and over again till
+# the length is 0
 def recursion(len):
     if len > 0:
         t.forward(len)
@@ -15,6 +17,5 @@ def recursion(len):
         t.left(45)
         recursion(len - 1)
 recursion(150)
+# makes turtle exit when you click it
 end.exitonclick()
-
-    
